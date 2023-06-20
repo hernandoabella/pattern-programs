1a3d51c440e72004118f6a9f4d7ff11968096b15
Hernando Abella <hernandoabella@gmail.com>
2023-06-20 03:39:59 -0500

    update

---
+++ script.js
@@ -0,0 +1,28 @@
+var copyButtons = document.querySelectorAll(".copyButton");
+
+copyButtons.forEach(function (copyButton) {
+  copyButton.addEventListener("click", function () {
+    var card = this.parentNode;
+    var codeSnippet = card.querySelector(".codeSnippet");
+
+    var range = document.createRange();
+    range.selectNode(codeSnippet);
+
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    try {
+      document.execCommand("copy");
+      copyButton.textContent = "¡Copiado!";
+    } catch (err) {
+      console.log("No se pudo copiar el código", err);
+    }
+
+    selection.removeAllRanges();
+  });
+
+  copyButton.addEventListener("mouseout", function () {
+    copyButton.textContent = "Copiar";
+  });
+});
